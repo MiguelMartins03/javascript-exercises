53c447d03701c4030abedc185ce43d7d00066340
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-06 19:55:15 +0100

    Complete exercise five of javascript-exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(initialNum, finalNum) {
+    let result = 0;
+    if((typeof initialNum === "number" && initialNum >= 0) && (typeof finalNum === "number" && finalNum >= 0)){
+        let i, max;
+        if(initialNum < finalNum){
+            i = initialNum;
+            max = finalNum;
+        }else{
+            i = finalNum;
+            max = initialNum;
+        }
+        for(;i <= max; i++){
+            result += i;
+        }
+    }else{
+        result = "ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line
