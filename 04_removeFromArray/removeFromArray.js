41307bd91492a0b38f7181784a923509db716607
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-06 19:15:30 +0100

    Complete exercise four of javascript-exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    for(let i = 0; i < itemsToRemove.length; i++){
+        let indexToRemove = array.indexOf(itemsToRemove[i]);
+        if(indexToRemove !== -1){
+            array.splice(indexToRemove, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
