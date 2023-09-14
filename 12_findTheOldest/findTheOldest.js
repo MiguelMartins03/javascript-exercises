a245df869601d8684403da15d9a2964645f7e233
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-14 19:30:42 +0100

    Complete exercise twelve of javascript-exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.sort((firstPerson, secondPerson) => {
+        if(!firstPerson.yearOfDeath) firstPerson.yearOfDeath = new Date().getFullYear();
+        if(!secondPerson.yearOfDeath) secondPerson.yearOfDeath = new Date().getFullYear();
+        return (secondPerson.yearOfDeath - secondPerson.yearOfBirth) - (firstPerson.yearOfDeath - firstPerson.yearOfBirth);
+    })[0];
 };
 
 // Do not edit below this line
