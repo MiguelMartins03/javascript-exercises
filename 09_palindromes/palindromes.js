a5094737c6179bc150b91e56d652086c00db87af
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-14 11:17:04 +0100

    Complete exercise nine of javascript-exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let originalString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return originalString === originalString.split("").reverse().join("");
 };
 
 // Do not edit below this line
